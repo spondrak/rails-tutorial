f62d35668be138b83cca9d35ae039528b82634bd
Alex Vondrak <avondrak@truex.com>
2020-08-12 22:13:35 -0700

    Start your own span

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -4,4 +4,12 @@ class ApplicationJob < ActiveJob::Base
 
   # Most jobs are safe to ignore if the underlying records are no longer available
   # discard_on ActiveJob::DeserializationError
+
+  around_perform do |job, block|
+    Honeycomb.start_span(name: 'active_job') do |span|
+      span.add_field 'active_job.class', job.class.name
+      span.add_field 'active_job.arguments', job.arguments
+      block.call
+    end
+  end
 end
